a84636ed55830b0255a8a5782459a9759c6af88a
bhargav-rishi <rishibhargav.m@gmail.com>
2025-02-26 10:10:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ADA_Week3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello, World!");
+
+
+            int x;
+            x = 3;
+        }
+    }
+}
